b83d4e4f164fa0e09d5dfce36f90b8fd1f94ebaf
Alterock13 <pierret.arn2@gmail.com>
2018-06-05 18:16:13 +0200

    Rapport3

--- simuOtherMethod.R
+++ simuOtherMethod.R
@@ -1,7 +1,7 @@
 ## Simul
   
   simulationServeur2 = function(lambda,mu,N,p1,p2,p3,duration,nbServeurs){
-    ## N : capacité maximale de stockage (en service + dans la file d'attente)
+    ## N : capacité maximale de stockage (en service + file d'attente)
     ## p1,p2,p3 : proportion de priorités (p1+p2+p3 = 1)
     ## lambda : intensité, lambda>0
     ## 1/mu : temps de traitement, mu>0
@@ -12,14 +12,18 @@
     totalTime = duration   # duration of simulation
     expTime = 0 # simulation time
     
-    N = N * nbServeurs # on augmente la taille de la file globale en fonction du nombre de serveurs dispos
+    tempsArrivee = 0 # Temps pour la reception suivante
+    tempsService = 0 # Temps pour le service suivant
     
+    N = N * nbServeurs # on augmente la taille de la file globale en fonction du nombre de serveurs disponibles
+
     ## Pour les arrivees
     k1 = 0 # nombre de requetes dans la queue de P1
     k2 = 0 # nombre de requetes dans la queue de P2
     k3 = 0 # nombre de requetes dans la queue de P3
     k = 0 # nombre de requetes totales 
-    
+
+    ### Compteurs    
     compteurTraitesk1 = 0 # nombre des requetes 1 traitees successivement
     compteurTraitesk2 = 0 # nombre des requetes 2 traites successivement
     compteurTraitesk3 = 0 # nombre des requetes 1 traitees successivement
@@ -32,23 +36,17 @@
     compteurPerduesk2 = 0 # nombre des requetes 2 perdues
     compteurPerduesk3 = 0 # nombre des requetes 1 perdues
     
-    tempsArrivee = 0 # Temps pour la reception suivante
-    tempsService = 0 # Temps pour le service suivant
-    
     paquetsRecus = 0
     paquetsTraites = 0
     paquetsPerdus = 0
     
-    creditq1 = 4
-    creditq2 = 4
-    creditq3 = 4
     
     while(expTime<totalTime){
+      # k==0 pour le cas ou il n'y a pas de paquets à traiter
       if (tempsArrivee< tempsService || k == 0) {
         paquetsRecus = paquetsRecus + 1
-        ## On associe a la requete une priorite
+        ## On associe a la requete une priorite par une loi uniforme
         randomPriority = sample(1:100,1)
-        
         if(randomPriority < p1*100){
           prochaineRequete = rexp(1,lambda)
           if(k==N){
@@ -90,23 +88,25 @@
         
       
       }else
+        ## Partie traitement des requetes
         {
           prochainService = rexp(1,mu)
           tempsService = tempsService + prochainService
+          ## Gestion multi-serveurs
           for (i in 0:nbServeurs) {
-            if (k1 > 0 && creditq1>0){
+            if (k1 > 0){
               k1 = k1 - 1
               paquetsTraites = paquetsTraites + 1
               compteurTraitesk1 = compteurTraitesk1 +1
               
             }
-            else if (k2 > 0 && creditq2>0){
+            else if (k2 > 0){
               k2 = k2 - 1
               paquetsTraites = paquetsTraites + 1
               compteurTraitesk2 = compteurTraitesk2+1
               
             }
-            else if (k3 > 0 && creditq3>0){
+            else if (k3 > 0){
               k3 = k3 - 1
               paquetsTraites = paquetsTraites + 1
               compteurTraitesk3 = compteurTraitesk3 +1
@@ -118,31 +118,46 @@
       }
     }
     
-    print ("#########")
+    print ("## DEBUT SIMULATION ")
+    print ("Paquets reçus :")
     print(paquetsRecus)
+    print ("Paquets traités :")
     print(paquetsTraites)
+    print ("Paquets perdus :")
     print(paquetsPerdus)
+    print ("Paquets totaux (calculés) :")
     print(paquetsTraites+paquetsPerdus+k)
     print ("#########")
+    print ("Paquets reçus P1 :")
     print(compteurRecuesk1)
+    print ("Paquets reçus P2 :")
     print(compteurRecuesk2)
+    print ("Paquets reçus P3 :")
     print(compteurRecuesk3)
+    print ("Paquets reçus (calculés):")
     print(compteurRecuesk1+compteurRecuesk2+compteurRecuesk3)
     print ("#########")
+    print ("Paquets traités P1 :")
     print(compteurTraitesk1)
+    print ("Paquets traités P2:")
     print(compteurTraitesk2)
+    print ("Paquets traités P3:")
     print(compteurTraitesk3)
     print ("#########")
+    print ("Paquets perdus P1:")
     print(compteurPerduesk1)
+    print ("Paquets perdus P1:")
     print(compteurPerduesk2)
+    print ("Paquets perdus P1:")
     print(compteurPerduesk3)
-    
+    print("## FIN SIMULATION")
     
   
   }
   
+  ## Test de l'augmentation du nombre de serveurs
   simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,1)
-  #simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,2)
-  #simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,4)
-  #simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,8)
+  simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,2)
+  simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,4)
+  simulationServeur2(6,1,20,0.5,0.3,0.2,10^4,8)
   
